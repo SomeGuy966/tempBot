42c3f9f0d61da3b0273590e2f414bb21df0c56fb
Kevin Ge <kevin@honeyrealty.ai>
2024-12-26 14:35:05 -0500

    confirmed prod test via hello world text change

--- functions/main.py
+++ functions/main.py
@@ -11,6 +11,6 @@ initialize_app(cred)
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+    return https_fn.Response("Hello world! Let's see if this works in production")
 
 
